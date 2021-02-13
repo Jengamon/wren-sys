aceb488298973f88acc472d3d2e44dcf47399f4c
Sam Morrison <sam@sam.vg>
2021-02-13 09:49:33 -0600

    Rename /src to /vm to better reflect Wren folder structure

--- build.rs
+++ build.rs
@@ -8,27 +8,27 @@ fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
 
     cc::Build::new()
-        .files(glob::glob("src/*.c").expect("Failed to glob c files").filter_map(|x| x.ok()))
+        .files(glob::glob("vm/*.c").expect("Failed to glob c files").filter_map(|x| x.ok()))
         .files(glob::glob("optional/*.c").expect("Failed to glob c files").filter_map(|x| x.ok()))
         .include("include")
         .include("optional")
-        .include("src")
+        .include("vm")
         .compile("wren");
-    
+
     let bindings = bindgen::Builder::default()
         .detect_include_paths(true)
         .header("wrapper.h")
         .whitelist_var("WREN.*")
         .whitelist_type("Wren.*")
         .whitelist_function("wren.*")
-        .clang_arg("-Isrc")
+        .clang_arg("-Ivm")
         .clang_arg("-Iinclude")
         .clang_arg("-Ioptional")
         .rustfmt_bindings(true)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings.");
-    
+
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
